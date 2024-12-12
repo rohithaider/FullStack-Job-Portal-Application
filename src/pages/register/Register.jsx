6df872e62094014d18a12315a97f0aa167120ca6
mohtashim haider <rohithaider@gmail.com>
2024-12-12 20:24:41 +0600

    Password validation is done

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,23 +1,44 @@
 import Lottie from "lottie-react";
-import React from "react";
-import registerLottie from "../../assets/lottie/register.json"
+import React, { useState } from "react";
+import registerLottie from "../../assets/lottie/register.json";
 
 function Register() {
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+
+    const passwordRegex = /^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{6,}$/;
+
+    //password validation
+
+    if (!passwordRegex.test(password)) {
+      setErrorMessage(
+        "Password must be at least 6 characters long, include at least one uppercase letter, and one number."
+      );
+      return;
+    }
+    setErrorMessage("");
+  };
+
   return (
     <div className="hero bg-base-200 min-h-screen">
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left lg:w-5/12">
-          
           <Lottie animationData={registerLottie}></Lottie>
         </div>
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <h1 className="text-5xl font-bold text-center mt-4">Register now!</h1>
-          <form className="card-body">
+          <form onSubmit={handleSubmit} className="card-body">
             <div className="form-control">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
+                name="email"
                 type="email"
                 placeholder="email"
                 className="input input-bordered"
@@ -29,11 +50,15 @@ function Register() {
                 <span className="label-text">Password</span>
               </label>
               <input
+                name="password"
                 type="password"
                 placeholder="password"
                 className="input input-bordered"
                 required
               />
+              {errorMessage && (
+                <p className="text-red-500 text-sm mt-2">{errorMessage}</p>
+              )}
               <label className="label">
                 <a href="#" className="label-text-alt link link-hover">
                   Forgot password?
